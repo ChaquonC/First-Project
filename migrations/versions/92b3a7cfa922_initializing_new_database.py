9960454e5c22e81ded34c02753bd0b23cf5ef81f
Chaquon <chaquonc@gmail.com>
2024-06-06 14:54:06 -0500

    fixed error with moves model

--- migrations/versions/92b3a7cfa922_initializing_new_database.py
+++ migrations/versions/92b3a7cfa922_initializing_new_database.py
@@ -1,8 +1,8 @@
 """initializing new database
 
-Revision ID: e59db1e1e960
+Revision ID: 92b3a7cfa922
 Revises: 
-Create Date: 2024-06-06 14:41:46.246468
+Create Date: 2024-06-06 14:52:07.288705
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'e59db1e1e960'
+revision = '92b3a7cfa922'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -50,7 +50,7 @@ def upgrade():
     )
     op.create_table('moves',
     sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('character_id', sa.String(), nullable=False),
+    sa.Column('character_id', sa.Integer(), nullable=False),
     sa.Column('move_type', sa.Enum('fire', 'ice', 'water', 'earth', 'wind', 'electric', 'slash', 'gun', 'bash', 'consume', name='move_types'), nullable=False),
     sa.Column('name', sa.String(length=100), nullable=False),
     sa.Column('created_at', sa.DateTime(timezone=True), server_default=sa.text('(CURRENT_TIMESTAMP)'), nullable=True),
